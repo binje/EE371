a556198c829c006d03f323074484664e878ec6ef
lamemmett <lam.emmett@gmail.com>
2015-01-18 17:12:42 -0800

    Update to syncDown counter
    out[2] logic broken

--- syncDown.sv
+++ syncDown.sv
@@ -2,12 +2,18 @@ module syncDown(clk, rst, out);
 	input clk, rst;
 	output [3:0] out;
 	
-	wire qbar0, qbar1, qbar2, qbar3;
+	wire d0, d1, d2, d3, qbar0, qbar1, qbar2, qbar3;
 	
-	DFlipFlop bit0 (out[0], qbar0, qbar0, clk, rst);
-	DFlipFlop bit1 (out[1], qbar1, qbar1, out[0], rst);
-	DFlipFlop bit2 (out[2], qbar2, qbar2, out[1], rst);
-	DFlipFlop bit3 (out[3], qbar3, qbar3, out[2], rst);
+	assign d0 = ~out[0];
+	assign d1 = ~(out[1] ^ out[0]);
+	assign d2 = (out[2] & out[0]) + (out[2] & out[1]) + (~out[2] & ~out[1] & ~out[0]);
+ 	assign d3 = (~out[3] & ~out[2] & ~out[1] & ~out[0]) + (out[3] & out[2]) +
+					(out[3] & out[0]) + (out[3] & out[1]);
+	
+	DFlipFlop bit0 (out[0], qbar0, d0, clk, rst);
+	DFlipFlop bit1 (out[1], qbar1, d1, clk, rst);
+	DFlipFlop bit2 (out[2], qbar2, d2, clk, rst);
+	DFlipFlop bit3 (out[3], qbar3, d3, clk, rst);
 endmodule 
 
 module syncDown_testbench();
