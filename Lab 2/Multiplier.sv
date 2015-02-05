12ed18b026ce53b385847b19bf4156d1d2ad800e
lamemmett <lam.emmett@gmail.com>
2015-02-04 18:21:56 -0800

    Update to multiplier

--- Lab 2/Multiplier.sv
+++ Lab 2/Multiplier.sv
@@ -1,4 +1,4 @@
-module MULTIPLY (X4register, Y4register, Out8register);
+module Multiplier (X4register, Y4register, Out8Register);
 	input [3:0] X4register, Y4register;
 	output [7:0] Out8Register;
 
@@ -6,33 +6,42 @@ module MULTIPLY (X4register, Y4register, Out8register);
 	reg [4:0] sum1, sum2, sumc1, sumc2;
 	
 
-	xny0 = X4Register & Y4register[0];
-	xny1 = X4Register & Y4register[1];
-	xny2 = X4Register & Y4register[2];
-	xny3 = X4Register & Y4register[3];
+	assign xny0 = X4register & Y4register[0];
+	assign xny1 = X4register & Y4register[1];
+	assign xny2 = X4register & Y4register[2];
+	assign xny3 = X4register & Y4register[3];
 
-	fullAdder fa01 (xny0[1],	xny1[0], 0, 	  Out8Register[1], sumc1[0]);
-	fullAdder fa02 (xny0[2], xny1[1], xny2[0], sum1[0], 	   sumc1[1]);
-	fullAdder fa03 (xny0[3], xny1[2], xny2[1], sum1[1], 	   sumc1[2]);
-	fullAdder fa04 (xny1[3], xny2[2], xny3[1], sum1[2], 	   sumc1[3]);
-	fullAdder fa05 (0, 	xny2[3], xny3[2], sum1[3], 	   sumc1[4]);
-	sum1[4] = xny3[3];
+	fullAdder fa01 (.A(xny0[1]),	.B(xny1[0]), .C(1'b0), .sum(Out8Register[1]), .co(sumc1[0]));
+	fullAdder fa02 (.A(xny0[2]), .B(xny1[1]), .C(xny2[0]), .sum(sum1[0]), .co(sumc1[1]));
+	fullAdder fa03 (.A(xny0[3]), .B(xny1[2]), .C(xny2[1]), .sum(sum1[1]), .co(sumc1[2]));
+	fullAdder fa04 (.A(xny1[3]), .B(xny2[2]), .C(xny3[1]), .sum(sum1[2]), .co(sumc1[3]));
+	fullAdder fa05 (.A(1'b0), 	.B(xny2[3]), .C(xny3[2]), .sum(sum1[3]), 	   .co(sumc1[4]));
+	assign sum1[4] = xny3[3];
 	
-	fullAdder fa10 (sum1[0], sumc1[0], 0, Out8Register[2], sumc2[0]);
-	fullAdder fa11 (sum1[1], sumc1[1], xny3[0], sum2[0], sumc2[1]);
-	fullAdder fa12 (sum1[2], sumc1[2], 0, sum2[1], sumc2[2]);
-	fullAdder fa13 (sum1[3], sumc1[3], 0, sum2[2], sumc2[3]);
-	fullAdder fa14 (sum1[4], sumc1[4], 0, sum2[3], sumc2[4]);
-	sum2[4] = 0;
+	fullAdder fa10 (.A(sum1[0]), .B(sumc1[0]), .C(1'b0), .sum(Out8Register[2]), .co(sumc2[0]));
+	fullAdder fa11 (.A(sum1[1]), .B(sumc1[1]), .C(xny3[0]), .sum(sum2[0]), .co(sumc2[1]));
+	fullAdder fa12 (.A(sum1[2]), .B(sumc1[2]), .C(1'b0), .sum(sum2[1]), .co(sumc2[2]));
+	fullAdder fa13 (.A(sum1[3]), .B(sumc1[3]), .C(1'b0), .sum(sum2[2]), .co(sumc2[3]));
+	fullAdder fa14 (.A(sum1[4]), .B(sumc1[4]), .C(1'b0), .sum(sum2[3]), .co(sumc2[4]));
+	assign sum2[4] = 0;
 
-	
-	
-	
-
-	Out8Register[0] = xny0[0];
+	assign Out8Register[0] = xny0[0];
+	wire [7:0] temp;
+	adder8 addgate (.Output(temp), .op1({3'b0, sum2}), .op2({3'b0, sumc2}), .control(1'b0));
+	assign Out8Register[7:3] = temp[4:0];
 	//Out8Register[1] = xny0[1] & xny1[0];
 
 endmodule
 
+module Multiplier_testbench();
+	reg [3:0] X4register, Y4register;
+	wire [7:0] Out8Register;
 
-fullAdder(A, B, C, sum, co);
+	Multiplier test (.X4register, .Y4register, .Out8Register);
+	
+	initial begin
+		X4register = 4'b0010;
+		Y4register = 4'b0010;
+		#1000;
+	end
+endmodule;
